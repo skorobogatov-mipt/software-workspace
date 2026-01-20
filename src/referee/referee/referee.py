ad9cdc0a8e44bc0c16e2001ae1001af1294c0df5
jazzhands <egod09080908@yandex.ru>
2026-01-20 13:03:48 +0300

    added referee node

---
+++ src/referee/referee/referee.py
@@ -0,0 +1,4 @@
+class Referee():
+    def __init__(self) -> None:
+        pass
+
---
+++ src/referee/referee/referee_node.py
@@ -0,0 +1,11 @@
+import rclpy
+from rclpy.node import Node
+
+class RefereeNode(Node):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
