ec0556be5e614e7d116e21e9d94828d6bfcaaeee
jazzhands <egod09080908@yandex.ru>
2026-01-30 00:27:36 +0300

    minor fix

--- src/conveyor_controller/conveyor_controller/conveyor_controller.py
+++ src/conveyor_controller/conveyor_controller/conveyor_controller.py
@@ -23,7 +23,7 @@ class ConveyorController(Node):
         )
         return_msg.joint_names = ['conveyor/conveyor_motor']
         return_msg.positions = [self.speed]
-        for _ in range(10):
+        for _ in range(1000):
             self.speed_pub.publish(return_msg)
             time.sleep(0.01)
         self.get_logger().info(f'SPEED SET FOR: {new_speed}')
